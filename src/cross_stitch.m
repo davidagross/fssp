27fad941b22ccf929e190ae7e3bda3f359fd1a10
David Gross <david.alexander.gross@gmail.com>
2013-01-02 00:00:06 -1000

    updated to recreated original design
    had to modify default behavior and colormap scaling in order to
    reproduce original results.

--- src/cross_stitch.m
+++ src/cross_stitch.m
@@ -1,4 +1,4 @@
-function varargout = cross_stitch(varargin)
+function B = cross_stitch(A,dims,cMap,method,zeroLevel)
 % CROSS_STITCH takes in a picture and outputs a color coded matrix
 %
 % *Usage*
@@ -14,23 +14,36 @@ function varargout = cross_stitch(varargin)
 %
 %   B = cross_stitch(A,SIZE,COLORMAP,METHOD) takes in the matrix
 %   (or RGB array) A and outputs a matrix of size SIZE scaled to the
-%   colormap COLORMAP and uses the provided method (shrink or crop)
+%   colormap COLORMAP and uses the provided method (crop)
+%
+%   B = cross_stitch(A,SIZE,COLORMAP,METHOD,ZEROLEVEL) takes in the matrix
+%   (or RGB array) A and outputs a matrix of size SIZE scaled to the
+%   colormap COLORMAP and uses the provided method (crop) and setting the
+%   zero-level for masking effects.
 %
 % *Authorship*
 %   Created on August 2, 2011 by David Gross
-%   Last Modified on August 2, 2011 by David Gross
+%   Last Modified on December 31, 2012 by David Gross
 %
 % See Also
 %   fssp
 
 %% Check Inputs
-if nargin<5, zeroLevel = 0; end
-if nargin<4, method = 'crop'; else method = varargin{4}; end
-if nargin<3, cMap = [1,.4,.4;.2,.2,.2;0,.5,1]; else cMap = varargin{3}; end
-if nargin<2, dims = [69,33]; else dims = varargin{2}; end
-if nargin<1, error('cross_stitch:InvalidInputs', ...
-        'Must provide an image to process'); else A = varargin{1}; end
+if ~exist('method','var') || isempty(method)
+    method = 'crop';
+end
+if ~exist('cMap','var') || isempty(cMap)
+    cMap = [1,.4,.4;.2,.2,.2;0,.5,1];
+end
+if ~exist('dims','var') || isempty(dims)
+    dims = [69,33];
+end
+if nargin<1
+    error('cross_stitch:InvalidInputs', ...
+        'Must provide an image to process');
+end
 %% Size the Image
+A = flipud(A);
 B = zeros(dims);
 switch method
     case 'crop'
@@ -53,11 +66,12 @@ switch method
             'Method must be "crop".');
 end
 %% Color the image
-% B = B - min(B(:));
-% B = B./max(B(:)) + zeroLevel;
-% for i = 1:numel(B)
-%    B(i) = floor(B(i)*(size(cMap,1)-1));
-% end
+totalColors = unique(B);
+diffs = diff(linspace(min(totalColors),max(totalColors),size(cMap,1)));
+cMap = [1 1 1 ; cMap];
+if ~exist('zeroLevel','var') || isempty(zeroLevel)
+    zeroLevel = min(totalColors)-diffs(1);
+end
 
 %% mask
 B(1,1) = zeroLevel; B(1,2) = zeroLevel; B(2,1) = zeroLevel;
@@ -65,12 +79,19 @@ B(1,end) = zeroLevel; B(1,end-1) = zeroLevel; B(2,end) = zeroLevel;
 B(end,end) = zeroLevel; B(end,end-1) = zeroLevel; B(end-1,end) = zeroLevel;
 B(end-7:end,1:12) = zeroLevel;
 
-%% Deal with outputs
-if nargout < 1
-    imagesc(flipud(B)); grid on; grid minor
-    set(gca,'xtick',0:5:33);
-    set(gca,'yticklabel',flipud(get(gca,'yticklabel')));
-    colormap(cMap); 
-    axis equal tight; 
-end
-if nargout == 1, varargout{1} = B; end
+%% plotting
+colormap(cMap);
+imagesc(flipud(B),[zeroLevel+1,max(totalColors)+1]);
+grid on; grid minor;
+axis equal tight;
+ax1 = gca;
+set(ax1,'xtick',0:5:dims(2),'ytick',0:5:dims(1));
+set(ax1,'yticklabel',fliplr(0:5:dims(1)+5));
+ax2 = axes(rmfield(get(ax1), { ...
+    'BeingDeleted','CurrentPoint','TightInset', ...
+    'Title','Type','XLabel','YLabel','ZLabel', ...
+    'Children'}));
+set(ax2,'XAxisLocation','top',...
+    'YAxisLocation','right',...
+    'Color','none');
+axes(ax1)

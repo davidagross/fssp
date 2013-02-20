6862eeb821cbd4239a7fe1113e2da8f3604cc870
David Gross <david.alexander.gross@gmail.com>
2013-02-19 22:19:34 -0800

    added math and case-cropping

--- src/iphone5_cross_stitch.m
+++ src/iphone5_cross_stitch.m
@@ -1,87 +1,54 @@
-function B = cross_stitch(A,dims,cMap,method,zeroLevel)
-% CROSS_STITCH takes in a picture and outputs a color coded matrix
+function B = iphone5_cross_stitch(A)
+% IPHONE5_CROSS_STITCH takes in a picture and outputs a cropped on
 %
-% *Usage*
-%   B = cross_stitch(A) takes in the matrix (or RGB array) A and outputs a
-%   matrix of size <<INSERT IPHONE CASE SIZE HERE>> scaled to the <<DEFAULT
-%   COLOR SCHEME>>
+%USAGE
+%   B = cross_stitch(A)
 %
-%   B = cross_stitch(A,SIZE) takes in the matrix (or RGB array) A and
-%   outputs a matrix of size SIZE scaled to the <<DEFAULT COLOR SCHEME>>
-%
-%   B = cross_stitch(A,SIZE,COLORMAP) takes in the matrix (or RGB array) A
-%   and outputs a matrix of size SIZE scaled to the colormap COLORMAP
-%
-%   B = cross_stitch(A,SIZE,COLORMAP,METHOD) takes in the matrix
-%   (or RGB array) A and outputs a matrix of size SIZE scaled to the
-%   colormap COLORMAP and uses the provided method (crop)
-%
-%   B = cross_stitch(A,SIZE,COLORMAP,METHOD,ZEROLEVEL) takes in the matrix
-%   (or RGB array) A and outputs a matrix of size SIZE scaled to the
-%   colormap COLORMAP and uses the provided method (crop) and setting the
-%   zero-level for masking effects.
-%
-% *Authorship*
-%   Created on August 2, 2011 by David Gross
-%   Last Modified on December 31, 2012 by David Gross
+%AUTHORSHIP
+%   Created by David A. Gross on 19 Feb 2013 at 6:30 PM
 %
 % See Also
-%   fssp
+%   fssp, mandelbroidery
 
-%% Check Inputs
-if ~exist('method','var') || isempty(method)
-    method = 'crop';
-end
-if ~exist('cMap','var') || isempty(cMap)
-    cMap = [1,.4,.4;.2,.2,.2;0,.5,1];
-end
-if ~exist('dims','var') || isempty(dims)
-    dims = [69,33];
-end
-if nargin<1
-    error('cross_stitch:InvalidInputs', ...
-        'Must provide an image to process');
-end
-%% Size the Image
-A = flipud(A);
-B = zeros(dims);
-switch method
-    case 'crop'
-        isSmallerOrEqual = size(A) <= dims;
-        if isSmallerOrEqual(1) && isSmallerOrEqual(2)
-            B((1:size(A,1))-round(size(A,1)/2)+round(size(B,1)/2), ...
-                (1:size(A,2))-round(size(A,2)/2)+round(size(B,2)/2)) = A;
-        elseif isSmallerOrEqual(1) && ~isSmallerOrEqual(2)
-            B((1:size(A,1))-round(size(A,1)/2)+round(size(B,1)/2),:) = ...
-                A(:,(1:size(B,2))-round(size(B,2)/2)+round(size(A,2)/2));
-        elseif ~isSmallerOrEqual(1) && isSmallerOrEqual(2)
-            B(:,(1:size(A,2))-round(size(A,2)/2)+round(size(B,2)/2),:) = ...
-                A((1:size(B,1))-round(size(B,1)/2)+round(size(A,1)/2),:);
-        elseif ~isSmallerOrEqual(1) && ~isSmallerOrEqual(2)
-            B = A((1:size(B,1))-round(size(B,1)/2)+round(size(A,1)/2), ...
-                (1:size(B,2))-round(size(B,2)/2)+round(size(A,2)/2));
-        end
-    otherwise
-        error('cross_stitch:InvalidInputs', ...
-            'Method must be "crop".');
-end
-%% Color the image
-totalColors = unique(B);
-diffs = diff(linspace(min(totalColors),max(totalColors),size(cMap,1)));
-cMap = [1 1 1 ; cMap];
-if ~exist('zeroLevel','var') || isempty(zeroLevel)
-    zeroLevel = min(totalColors)-diffs(1);
-end
+% configure colors
+cMapFun = @autumn;
+flipFirst = true;
+numColors = 6;
 
-%% mask
-B(1,1) = zeroLevel; B(1,2) = zeroLevel; B(2,1) = zeroLevel;
-B(1,end) = zeroLevel; B(1,end-1) = zeroLevel; B(2,end) = zeroLevel;
-B(end,end) = zeroLevel; B(end,end-1) = zeroLevel; B(end-1,end) = zeroLevel;
-B(end-7:end,1:12) = zeroLevel;
+% transfer (this isn't fancy because we aren't resizing)
+if ~isequal(size(A),[77,35])
+    error('This isn''t an iPhone 5 case design');
+end
+B = A;
+dims = size(B);
 
-%% plotting
-colormap(cMap);
-imagesc(flipud(B),[zeroLevel+1,max(totalColors)+1]);
+% masking
+for r = 1:3
+    B = rot90(B);
+    B(1,1) = nan; B(1,2) = nan;
+    B(2,1) = nan; 
+end
+B = rot90(B);
+
+B(1,1:13) = nan;
+B(2,1:15) = nan;
+B(3,1:15) = nan;
+B(4,1:16) = nan;
+B(5,1:16) = nan;
+B(6,1:16) = nan;
+B(7,1:16) = nan;
+B(8,2:16) = nan;
+B(9,2:15) = nan;
+B(10,3:15) = nan;
+B(11,4:13) = nan;
+
+% plotting
+if flipFirst
+    cMap = [flipud( cMapFun(numColors) ); cMapFun(numColors); 0 0 0];
+else
+    cMap = [cMapFun(numColors); flipud( cMapFun(numColors) ); 0 0 0];
+end
+imagescwithnan( B , cMap , [1 1 1] ); shading flat;
 grid on; grid minor;
 axis equal tight;
 ax1 = gca;
@@ -95,3 +62,34 @@ set(ax2,'XAxisLocation','top',...
     'YAxisLocation','right',...
     'Color','none');
 axes(ax1)
+
+end
+
+% modified from http://stackoverflow.com/questions/8481324/contrasting-color-for-nans-in-imagesc
+function h = imagescwithnan(a,cm,nanclr)
+% IMAGESC with NaNs assigning a specific color to NaNs
+
+%# find minimum and maximum
+amin=min(a(:));
+amax=max(a(:));
+%# size of colormap
+n = size(cm,1);
+%# color step
+dmap=(amax-amin)/n;
+
+%# standard imagesc
+him = imagesc(a);
+%# add nan color to colormap
+colormap([nanclr; cm]);
+%# changing color limits
+caxis([amin-dmap amax]);
+%# place a colorbar
+% hcb = colorbar;
+%# change Y limit for colorbar to avoid showing NaN color
+% ylim(hcb,[amin amax])
+
+if nargout > 0
+    h = him;
+end
+
+end
